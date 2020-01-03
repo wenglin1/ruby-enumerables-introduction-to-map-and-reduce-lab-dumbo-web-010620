d6fdbee9a1cf63519b5cc728175d72c95d3d9a4d
weng lin <wenglin@buffalo.edu>
2020-01-03 17:06:47 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,63 @@
-# My Code here....
+def map_to_negativize(source_array)
+  new_arr = source_array
+  i = 0
+  while new_arr[i] do
+    new_arr[i] *= -1
+    i += 1
+  end
+  new_arr
+end
+
+def map_to_no_change(source_array)
+  return source_array
+end
+
+def map_to_double(source_array)
+  new_arr = source_array
+  i = 0
+  while new_arr[i] do
+    new_arr[i] *= 2
+    i += 1
+  end
+  new_arr
+end
+
+def map_to_square(source_array)
+  new_arr = source_array
+  i = 0
+  while new_arr[i] do
+    new_arr[i] = new_arr[i] ** 2
+    i += 1
+  end
+  new_arr
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  total = starting_point
+  i = 0
+  while source_array[i] do
+    total += source_array[i]
+    i += 1
+  end
+  total
+end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    if source_array[i] == false || source_array[i] == nil
+      return false
+    end
+    i += 1
+  end
+  return true
+end
+
+def reduce_to_any_true(source_array)
+  i = 0
+  while i < source_array.length do
+    return true if source_array[i] == true
+    i += 1
+  end
+  return false
+end
