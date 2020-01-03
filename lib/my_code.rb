9b18ad9a04be28da8df9d7653ba57da113a03192
weng lin <wenglin@buffalo.edu>
2020-01-03 17:05:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,7 +45,7 @@ end
 def reduce_to_all_true(source_array)
   i = 0
   while i < source_array.length do
-    if source_array[i] == false || source_array[i] == nil
+    if source_array[i] == false
       return false
     end
     i += i
